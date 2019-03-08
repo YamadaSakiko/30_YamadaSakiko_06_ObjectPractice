ff80144e6e5da1b932603e9f640febebeed4a9ef
山田早紀子 <s1105257@gmail.com>
2019-03-09 03:52:29 +0900

    第６回課題演習問題

---
+++ ObjectPractice-java2/src/com/company/ObjectPractice04.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class ObjectPractice04 {
+    public static void main(String[] args) {
+        Person person=new Person("山田","ドイツ");
+        person.selfIntroduction();
+
+    }
+}
